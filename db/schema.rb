4f1b3a1967183ea089a9fbe9117dfe4186245883
munetomo <fishmans0120@gmail.com>
2020-07-28 14:36:54 +0900

    rails db:migrate(comments-table)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_021541) do
+ActiveRecord::Schema.define(version: 2020_07_28_051246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "text"
